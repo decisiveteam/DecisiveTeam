aa3fcfadb0a63041a3bd7cd7eb394ab67ed51302
Dan Allison <dan.allison@protonmail.com>
2023-04-18 20:12:14 -0700

    Add tag links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def with_tag_links(text, team_id)
+    sanitize(text).gsub(Tag.pattern) do |match|
+      "<a href='/teams/#{team_id}/tags/#{$1}'>#{match}</a>"
+    end.html_safe
+  end
 end

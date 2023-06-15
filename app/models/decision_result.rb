a5b914ea47fab9deda74daa50a3d10c5cd511cc5
Dan Allison <dan@decisive.team>
2023-06-15 02:05:26 +0000

    Highlight random_id relevance in results

--- app/models/decision_result.rb
+++ app/models/decision_result.rb
@@ -1,4 +1,19 @@
 class DecisionResult < ApplicationRecord
   self.primary_key = "option_id"
   self.table_name = "decision_results" # view
+
+  def get_sorting_factor(other_result)
+    if self.approved_yes != other_result.approved_yes
+      'approved_yes'
+    elsif self.stars != other_result.stars
+      'stars'
+    else
+      'random_id'
+    end
+  end
+
+  def is_sorting_factor?(other_result, factor)
+    return false if other_result.nil?
+    get_sorting_factor(other_result) == factor
+  end
 end

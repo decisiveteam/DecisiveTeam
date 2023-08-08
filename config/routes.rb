8ab60d2f9e4d6a56f8cde21bbb54dd4ac230b27d
Dan Allison <dan@decisive.team>
2023-08-08 00:05:41 +0000

    Change "new_decision" route to "decide"

--- config/routes.rb
+++ config/routes.rb
@@ -46,11 +46,13 @@ Rails.application.routes.draw do
   end
 
   get '/new_team' => 'teams#new'
+  get '/decide' => 'decisions#new'
+  get '/decision/:decision_uid' => 'decisions#show_by_uid'
   resources :teams, only: [:create, :index, :show] do
     get '/invite' => 'teams#invite'
     get '/join/:invite_code' => 'teams#join'
     get '/join/:invite_code/confirm' => 'teams#confirm_invite'
-    get '/new_decision' => 'decisions#new'
+    get '/decide' => 'decisions#new'
     resources :decisions, only: [:create, :show] do
       get '/results.html' => 'decisions#results_partial'
       get '/options.html' => 'decisions#options_partial'
